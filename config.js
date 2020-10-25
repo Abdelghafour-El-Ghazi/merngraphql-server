a8294cfd9015a699badf942462704c4fb0840b2b
Abdelghafour EL GHAZI <abdelghafour.elghazi@gamil.com>
2020-10-25 19:50:39 +0100

    add config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://Abdel:2Cbf2XpvEnrTMFKN@cluster0.kkoni.mongodb.net/socialmedia?retryWrites=true&w=majority',
+    SECRET_KEY:'this is  our little secret'
+}
